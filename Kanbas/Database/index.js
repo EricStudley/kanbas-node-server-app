5d32ea87725639f9cec3e3c7ff349c7145acfe8b
Eric Studley <eric.studley@gmail.com>
2024-07-29 23:10:59 -0400

    Fixing reference to deleted file.

--- Kanbas/Database/index.js
+++ Kanbas/Database/index.js
@@ -1,5 +1,4 @@
-import courses from "./courses.js";
 import assignments from "./assignments.js";
 import grades from "./grades.js";
 import enrollments from "./enrollments.js";
-export default { courses, assignments, grades, enrollments };
+export default { assignments, grades, enrollments };
